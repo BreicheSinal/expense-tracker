472bf7c8942beb0695bbf43d6b5619a8a4fadc25
BreicheSinal <sinalbreiche@gmail.com>
2024-11-02 21:37:39 +0200

    Added input validation

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,2 +1,9 @@
+// INDEX PAGE
+const loginBttn = document.getElementById("loginBttn");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const errorMsg = document.getElementById("errorMsg");
+
+// EXPENSE TRACKER PAGE
 const expenseBttn = document.getElementById("expenseBttn");
 const incomeBttn = document.getElementById("incomeBttn");
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,30 @@
-/*incomeBttn.addEventListener("click", () => {
-  window.location.href = "./pages/income.html";
-});
+loginBttn.addEventListener("click", () => {
+  let usernameInput = username.value;
+  let passwordInput = password.value;
 
-console.log("hiii");
-*/
+  console.log(usernameInput);
+  console.log(passwordInput);
+
+  if (usernameInput == "" && passwordInput == "") {
+    errorMsg.style.display = "block";
+    errorMsg.innerHTML = "Please Enter Username/Password";
+    console.log("im here");
+    setTimeout(() => {
+      errorMsg.style.display = "none";
+    }, 1500);
+  } else if (usernameInput == "") {
+    errorMsg.style.display = "block";
+    errorMsg.innerHTML = "Please Enter Username";
+    console.log("im here");
+    setTimeout(() => {
+      errorMsg.style.display = "none";
+    }, 1500);
+  } else if (passwordInput == "") {
+    errorMsg.style.display = "block";
+    errorMsg.innerHTML = "Please Enter Password";
+    console.log("im here");
+    setTimeout(() => {
+      errorMsg.style.display = "none";
+    }, 1500);
+  } else window.location.href = "./pages/expenseTracker.html";
+});
